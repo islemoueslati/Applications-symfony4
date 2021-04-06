82ec254b13a4eb6f2ac991d49dc2af97a87274f1
Islem Oueslati <islemoueslati2017@gmail.com>
2021-04-06 16:07:03 +0200

    Manipulation twig , bootstrap, js , css

---
+++ src/Controller/PersonnageController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PersonnageController extends AbstractController
+{
+    /**
+     * @Route("/", name="personnage")
+     */
+    public function index(): Response
+    {
+        return $this->render('personnage/index.html.twig');
+    }
+    /**
+     * @Route("/persos", name="persos")
+     */
+    public function persos(): Response
+    {
+        return $this->render('personnage/persos.html.twig');
+    }
+}
